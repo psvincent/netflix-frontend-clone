17c143439c83b1a89da5f02b04ab039a7235da6d
psvincent12 <psvincent12@gmail.com>
2022-07-13 15:44:37 -0500

    Added Nav with api calls

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '877a8077b7922c5dc3642e404c0cd1b3';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;
